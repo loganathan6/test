bbaf58dca1d9e351a119a2ae71c3404946be364b
k4m35h <1234.kamesh@gmail.com>
2018-01-05 16:36:03 +0530

    Create new_file_min.py

---
+++ new_file_min.py
@@ -0,0 +1,5 @@
+n = raw_input().split(" ")
+data = [0] + map(int,raw_input().split(" "))
+for i in range( int(n[1]) ):
+    pos = map(int,raw_input().split(" "))
+    print min(data[pos[0]:pos[1]+1])
