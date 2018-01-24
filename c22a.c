74145e62e739938154c0409673cb90ef6913f7a1
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-01-24 16:36:16 +0530

    Rename c22.c to c22a.c

---
+++ c22a.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a[15];
+int v,i,m;
+printf("enter the value");
+scanf("%d",&v);
+printf("enter the array");
+for(i=0;i<v;i++);
+{
+scanf("%d",&a[i])
+}
+max=max[0]
+for(i=0;i,v;i++)
+{
+if(max a[i])
+max=a[i];
+pritf("enter the maxmium value");
+}
+}
