893d1ed71b101f46ed8aab692e381356c58ec47d
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-01-24 16:12:48 +0530

    Create c27.c

---
+++ c27.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int i,j,k,c[100],n,t;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&c[i]);
+    }
+    for(j=1;j<=n;j++)
+    {
+        for(k=j+1;k<=n;k++)
+        {
+            if(b
+            c[j]>c[k])
+            {
+                t=c
+                c[j];
+                c[j]=c[k];
+                b[k]=t;
+            }
+        }
+    }
+printf("%d",c[j/2]);
+}
