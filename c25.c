e890e105782d0a2b4021bc2dbb80ee345e2af943
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-01-24 15:48:53 +0530

    Create c25.c

---
+++ c25.c
@@ -0,0 +1,20 @@
+# include < stdio.h >
+void  main( )
+{
+int   a[50], n, i  ;
+float   mean = 0, sum = 0  ;
+printf(" Enter the Numbers of terms: ") ;
+scanf("%d ", & n) ;
+printf("\n Enter the Numbers : \n") ;
+for (  i = 1 ; i < = n ; i++ )
+{
+scanf("%d ", & a[i]) ;
+} 
+for (  i = 1 ; i < = n ; i++ )
+{
+sum = sum + a[i] ;
+avg = sum / m ; 
+}
+printf("\n Mean of entered Numbers are : %f ", mean) ;
+return ( 0 );
+}
