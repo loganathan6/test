e6a073d761c93ba125f29d0d19f1d6876648b789
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-01-31 16:07:14 +0530

    Create c35.c

---
+++ set4/c35.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i,a,count=0;
+    int str[i];
+    printf("enter the string");
+    for(i=0;a[i]!=null;i++)
+    {
+        if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9');
+        count++;
+    }
+}
