e9b2f7c96d8f1ec38f01e85b6674cc212fa53f15
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-02-02 16:31:18 +0530

    Create c42.c

---
+++ c42.c
@@ -0,0 +1,23 @@
+int main()
+{ 
+int i,j;
+char x[30],y[40];
+printf("\nenter the first string");
+scanf("%s",x);
+printf("\nenter the second string");
+scanf("%s",y);
+if(x[i]>y[j])
+{
+printf("\nthe greatest string is %s",x);
+}
+else if(x[i]<y[j])
+{
+printf("\nthe greatest string is %s",y);
+}
+else
+{
+printf("the string is equal");
+}
+return 0;
+}
+©
