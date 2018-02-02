4d060e0c5b73916dffe4e199c13f756a2a298ec7
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-02-02 16:21:10 +0530

    Create c41.c

---
+++ c41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int k,i;
+	char str[30];
+	printf("enter times\n");
+	scanf("%d",&k);
+	scanf("%c",&str[30]);
+	for(i=1;i<=k;i++)
+	{
+		printf("%k",str[30]);
+	}
+getch();
+}
