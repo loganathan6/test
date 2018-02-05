47d891334cc28a293dea162705d4d368e45517e3
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-02-05 15:43:02 +0530

    Create C43.C

---
+++ set4/C43.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+	char a[40],b[40];
+	printf("\n enter the 2 strings");
+	scanf("%s %s",&a,&b);
+	printf("\n concadenated string is %s%s",a,b);
+	return 0;
+  }
