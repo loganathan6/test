26f8ec18785438e031ffdb722fbc5f587949eb5f
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-02-12 15:59:39 +0530

    Create 1.c

---
+++ set7/1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[40]="laptop";
+    int k,s;
+    scanf("%d",&k);
+    for(s=0;s<k;s++)
+    {
+        printf("%c",a[s]);
+    }
+}    
