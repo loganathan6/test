ca1da42b5cf8fc4c051c702979dc350ca06754ce
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-02-19 16:28:19 +0530

    Create c6..c

---
+++ set7/c6..c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int b,i,count=1;
+	scanf("%d",&b);
+	for(i=2;i<=a/2;i++)
+	{
+		if(a%i==0)
+		{
+			c=0;
+			break
+		}
+	}
+if(c==1)
+printf("YES");
+else
+printf("NO");
+return 0;
