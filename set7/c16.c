1e1b8d89401cc410f920c0511820ce539e01772f
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-02-28 15:53:17 +0530

    Create c16.c

---
+++ set7/c16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n,c=0;
+   
+    printf ("Enter a number \n");
+    scanf ("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        c=c+1;
+    }
+    if (c==2)
+    printf ("number is prime");
+    else
+    printf ("number is composite");
+    };
