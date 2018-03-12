21bbe834a9cd35c75fd2bbd2f787ee59f9649af9
loganathan6 <35221219+loganathan6@users.noreply.github.com>
2018-03-12 16:03:56 +0530

    Create c81.c

---
+++ set7/c81.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a[100],b[100],c[100],i;
+    clrscr();
+    printf("enter the difference\n:");
+    
+    for(i=0;i<=100;i++)
+    {
+        scanf("%d%d",&a[i],&b[i]);
+    }
+      for(i=0;a[i]=NULL;i++)
+      {
+          c[i]=a[i]-b[i];
+          printf("%d\n",c[i]);
+          return 0;
+      }}
